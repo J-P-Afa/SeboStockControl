b6ff8dcb11d8b4272afe214b3e8791f4022d9602
J-P-Afa <jpafarelli@gmail.com>
2026-04-03 19:15:59 -0300

    refactor(stock): update estoque controller

--- backend/src/modules/stock/presentation/controllers/estoque.controller.ts
+++ backend/src/modules/stock/presentation/controllers/estoque.controller.ts
@@ -1,10 +1,22 @@
-import { Controller, Get, Param, ParseIntPipe, Inject } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Inject,
+  UseGuards,
+} from '@nestjs/common';
 import { ESTOQUE_REPOSITORY } from '../../domain/repositories/stock.repository.interface';
 import type { IStockRepository } from '../../domain/repositories/stock.repository.interface';
 import { GetEstoqueHistoryUseCase } from '../../application/use-cases/get-estoque-history.use-case';
-import { Result } from '../../../../common';
+import {
+  Result,
+  PermissionsGuard,
+  RequirePermission,
+} from '../../../../common';
 
 @Controller('estoques')
+@UseGuards(PermissionsGuard)
 export class EstoqueController {
   constructor(
     @Inject(ESTOQUE_REPOSITORY)
@@ -13,11 +25,13 @@ export class EstoqueController {
   ) {}
 
   @Get()
+  @RequirePermission('stock:read')
   async findAll() {
     return this.repo.findAll();
   }
 
   @Get('book/:bookId')
+  @RequirePermission('stock:read')
   async findByBook(@Param('bookId', ParseIntPipe) bookId: number) {
     const item = await this.repo.findByBookId(bookId);
     if (!item) {
@@ -30,6 +44,7 @@ export class EstoqueController {
   }
 
   @Get('book/:bookId/history')
+  @RequirePermission('stock:read')
   async getHistory(@Param('bookId', ParseIntPipe) bookId: number) {
     return this.getEstoqueHistoryUseCase.execute(bookId);
   }
