8860b207961053e758cdb4f98717f16086b74463
J-P-Afa <jpafarelli@gmail.com>
2026-04-02 21:05:37 -0300

    refactor(backend): export ResultInterceptor in common module

--- backend/src/common/index.ts
+++ backend/src/common/index.ts
@@ -2,3 +2,4 @@ export * from './interfaces';
 export * from './decorators';
 export * from './guards';
 export * from './filters/http-exception.filter';
+export * from './interceptors/result.interceptor';
